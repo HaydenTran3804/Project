966ccf92dde1f8fb98ad5812e64d878257ae5acc
tranan3804 <tranan3804@gmail.com>
2023-06-18 01:28:50 -0700

    changed file Space invader

--- Expense-Tracker.cpp
+++ Expense-Tracker.cpp
@@ -34,27 +34,29 @@ void del(vector<Tracker> &s,int pos){
 	}
 }
 int main(){
+vector<Tracker> list;
+int choice = 1;
 cout << setw(65) << "Expense Tracker"<< endl;
 cout << setw(53) <<"1: Add" << " " << "2: Delete" << " " << "3: Logout"<<endl;
 cout << setw(17) <<"Type" <<setw(30)<<"Name" <<setw(30)<<"Date"<<setw(28)<<"Amount"<<endl;
-vector<Tracker> list;
-int choice=1;
 while(1){
-	if(choice==1){
+	if(choice == 1){
 		Tracker s; cin >> s;
 		list.push_back(s);
-	} else if(choice==2){
+	} else if(choice == 2){
 		int pos;cin >> pos;
 		del(list,pos);
 	} else break;
 	system("cls");
-		cout << setw(65) << "Expense Tracker"<< endl;
+cout << setw(65) << "Expense Tracker"<< endl;
 cout << setw(53) <<"1: Add" << " " << "2: Delete" << " " << "3: Logout"<<endl;
 cout << setw(17) <<"Type" <<setw(30)<<"Name" <<setw(30)<<"Date"<<setw(28)<<"Amount"<<endl;
-	for(int i=0;i<list.size();i++){
-		cout <<list[i];
-	}
+for(int i=0;i<list.size();i++){
+		cout << list[i];
+}
+cout << "Choose type" << " ";
 cin >> choice;
 cin.ignore();
 }
+cout << endl << system("PAUSE");
 }
--- Space-Invader.cpp
+++ Space-Invader.cpp
@@ -2,25 +2,15 @@
 #include <conio.h>
 #include <windows.h>
 using namespace std;
-map<int,vector<int>> spacecraft;
-vector<pair<int,int>> bullet;
+char enermies[25][50];
+int dx[4]={1,-1,0,0};
+int dy[4]={0,0,1,-1};
+int score = 5;
+class Enemies{
+public:
 char enermies[25][50];
 vector<pair<int,int>> pos_ene;
 vector<pair<int,int>> ene_bullet;
-int dx[4]={1,-1,0,0};
-int dy[4]={0,0,1,-1};
-int score=5;
-void create(){
-	for(int i=22;i<24;i++){
-		 if(i==22){
-		spacecraft[i].push_back(25);
-		} else{
-			for(int j=24;j<27;j++){
-				spacecraft[i].push_back(j);
-			}
-		}
-	}
-}
 bool appearene(int n,int m){
 	for(int i=0;i<pos_ene.size();i++){
 		if(pos_ene[i].first==n && pos_ene[i].second==m){
@@ -37,8 +27,7 @@ void checkene(){
 			}
 		}
 	}
-	
-}
+	}
 void delenepos(int n,int m){
 	for(int i=0;i<pos_ene.size();i++){
 		if(pos_ene[i].first==n && pos_ene[i].second==m){
@@ -46,13 +35,55 @@ void delenepos(int n,int m){
 		}
 	}
 }
+bool check_ene_bullet(int n,int m){
+	for(int i=0;i<ene_bullet.size();i++){
+		if(ene_bullet[i].first==n && ene_bullet[i].second==m){
+			return true;
+		} if(ene_bullet[i].first>=24) 	ene_bullet.erase(ene_bullet.begin()+i,ene_bullet.begin()+i+1);
+	}
+	return false;
+}
+void set_ene_bullet(){
+int i=rand()%pos_ene.size() + 0;
+if(enermies[pos_ene[i].first+1][pos_ene[i].second]!='*' && enermies[pos_ene[i].first][pos_ene[i].second]=='*'){
+			ene_bullet.push_back({pos_ene[i].first+1,pos_ene[i].second});
+		}
+}
+void ene_bullet_fly(){
+	for(int i=0;i<ene_bullet.size();i++){
+		ene_bullet[i].first+=1;
+	}
+}
+bool check_ene_move(int i){
+	for(int k=0;k<pos_ene.size();k++){
+ if(enermies[pos_ene[k].first+dx[i]][pos_ene[k].second+dy[i]]=='#' || pos_ene[k].first + dx[i]>=8 ) return true;
+	}
+	return false;
+}
+};
+Enemies enemies;
+class Craft{
+public:
+map<int,vector<int>> spacecraft;
+vector<pair<int,int>> bullet;
+void create(){
+	for(int i=22;i<24;i++){
+		 if(i==22){
+		spacecraft[i].push_back(25);
+		} else{
+			for(int j=24;j<27;j++){
+				spacecraft[i].push_back(j);
+			}
+		}
+	}
+}
 bool checkbullet(int n,int m){
 	for(int i=0;i<bullet.size();i++){
 		if(bullet[i].first==n && bullet[i].second==m){
 			if(enermies[n][m]=='*'){
-				enermies[n][m]=' ';
+				enemies.enermies[n][m]=' ';
 				score++;
-			    delenepos(n,m);
+			    enemies.delenepos(n,m);
 				bullet.erase(bullet.begin()+i,bullet.begin()+i+1);
 				return false;
 			}
@@ -64,23 +95,15 @@ bool checkbullet(int n,int m){
 	}
 	return false;
 }
-bool check_ene_bullet(int n,int m){
-	for(int i=0;i<ene_bullet.size();i++){
-		if(ene_bullet[i].first==n && ene_bullet[i].second==m){
-			return true;
-		} if(ene_bullet[i].first>=24) 	ene_bullet.erase(ene_bullet.begin()+i,ene_bullet.begin()+i+1);
-	}
-	return false;
-}
 bool checkcraft(int n,int m){
 	for(auto x:spacecraft){
 		if(x.first==n){
 		for(int j=0;j<x.second.size();j++){
 			if(x.second[j]==m){
-				for(int k=0;k<ene_bullet.size();k++){
-					if(ene_bullet[k].first==n && ene_bullet[k].second==m){
+				for(int k=0;k<enemies.ene_bullet.size();k++){
+					if(enemies.ene_bullet[k].first==n && enemies.ene_bullet[k].second==m){
 					score-=1;
-					ene_bullet.erase(ene_bullet.begin()+k,ene_bullet.begin()+k+1);
+					enemies.ene_bullet.erase(enemies.ene_bullet.begin()+k,enemies.ene_bullet.begin()+k+1);
 					break;
 				}
 				}
@@ -90,30 +113,6 @@ bool checkcraft(int n,int m){
 	}
 }
 	return false;
-}
-void board(){
-	for(int i=0;i<25;i++){
-		cout << setw(40);
-		for(int j=0;j<50;j++){
-			if(i==0 || i==24 || j==0 || j==49){
-			 cout <<"#";
-		} else if(checkcraft(i,j)) {
-		cout <<'o';
-		} else if(checkbullet(i,j)){
-		cout <<'^';
-		} else if(check_ene_bullet(i,j)){
-			cout <<"v";
-		}
-		  else{
-   cout << enermies[i][j];
-}
-		
-	} 
-	cout << endl;
-	}
-	cout << "Score: " <<score << endl;
-	if(score>=10) cout <<setw(50) << "You Win";
-else cout <<setw(50) <<"You Lose";
 }
 void bulletfly(){
 	for(int i=0;i<bullet.size();i++){
@@ -127,55 +126,63 @@ void move(int dir){
 		}
 	}
 }
-void set_ene_bullet(){
-	int i=rand()%pos_ene.size() + 0;
-		if(enermies[pos_ene[i].first+1][pos_ene[i].second]!='*' && enermies[pos_ene[i].first][pos_ene[i].second]=='*'){
-			ene_bullet.push_back({pos_ene[i].first+1,pos_ene[i].second});
+};
+Craft craft;
+void board(){
+	for(int i=0;i<25;i++){
+		cout << setw(40);
+		for(int j=0;j<50;j++){
+			if(i==0 || i==24 || j==0 || j==49){
+			 cout <<"#";
+		} else if(craft.checkcraft(i,j)) {
+		cout <<'o';
+		} else if(craft.checkbullet(i,j)){
+		cout <<'^';
+		} else if(enemies.check_ene_bullet(i,j)){
+			cout <<"v";
 		}
-	
+		  else{
+   cout << enemies.enermies[i][j];
 }
-void ene_bullet_fly(){
-	for(int i=0;i<ene_bullet.size();i++){
-		ene_bullet[i].first+=1;
+		
+	} 
+	cout << endl;
 	}
-}
-bool check_ene_move(int i){
-	for(int k=0;k<pos_ene.size();k++){
-		if(enermies[pos_ene[k].first+dx[i]][pos_ene[k].second+dy[i]]=='#' || pos_ene[k].first + dx[i]>=8 ) return true;
-	}
-	return false;
+	cout << "Score: " <<score << endl;
+	if(score>=10) cout <<setw(50) << "You Win";
+else cout <<setw(50) <<"You Lose";
 }
 void ene_move(){
 	int i;
 	do{
  i=rand()%4+0;
-} while(check_ene_move(i));
-	int n=pos_ene.size();
+} while(enemies.check_ene_move(i));
+	int n=enemies.pos_ene.size();
 	vector<pair<int,int>> tmp;
 	for(int h=0;h<n;h++){
-		enermies[pos_ene[h].first][pos_ene[h].second]=' ';
-		tmp.push_back({pos_ene[h].first+dx[i],pos_ene[h].second+dy[i]});
+		enemies.enermies[enemies.pos_ene[h].first][enemies.pos_ene[h].second]=' ';
+		tmp.push_back({enemies.pos_ene[h].first+dx[i],enemies.pos_ene[h].second+dy[i]});
 	}
-	pos_ene.clear();
-	pos_ene=tmp;
+	enemies.pos_ene.clear();
+	enemies.pos_ene=tmp;
 		for(int h=0;h<n;h++){
-			checkbullet(pos_ene[h].first,pos_ene[h].second);
-			enermies[pos_ene[h].first][pos_ene[h].second]='*';
+			craft.checkbullet(enemies.pos_ene[h].first,enemies.pos_ene[h].second);
+			enemies.enermies[enemies.pos_ene[h].first][enemies.pos_ene[h].second]='*';
 	}
 	
 }
 
 
 int main(){
-create();
+craft.create();
 for(int i=0;i<25;i++){
 	for(int j=0;j<50;j+=1){
 		if(i==0 || i==24 || j==0 || j==49){
-			enermies[i][j]='#';
+			enemies.enermies[i][j]='#';
 		} else if(i>3 && i<8 && j%12==0 && j!= 48){
-		pos_ene.push_back({i,j});
-		enermies[i][j]='*';
-	} else enermies[i][j]=' ';
+		enemies.pos_ene.push_back({i,j});
+		enemies.enermies[i][j]='*';
+	} else enemies.enermies[i][j]=' ';
 }
 }
 int dir;
@@ -188,19 +195,19 @@ while(score<12 && score>0){
 			case 'a': dir=-1;break;
 		}
 
-		move(dir);
+		craft.move(dir);
 } else {
-	move(dir);
+	craft.move(dir);
 }
   if(time==4){
-bullet.push_back({21,spacecraft[22][0]+dir});
-set_ene_bullet();
+craft.bullet.push_back({21,craft.spacecraft[22][0]+dir});
+enemies.set_ene_bullet();
 }
 ene_move();
 	if(time==4) time=0;
 	time++;
-	bulletfly(); 
-    ene_bullet_fly();
+	craft.bulletfly(); 
+    enemies.ene_bullet_fly();
 
 		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),{0,0});
 }
